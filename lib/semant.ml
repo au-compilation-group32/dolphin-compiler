d8318aac084370180a440631996e9918b3791b77
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-10-02 13:05:40 +0200

    implement infertype for binop and lval

--- lib/semant.ml
+++ lib/semant.ml
@@ -1,5 +1,5 @@
-module TAst = TypedAst
 module Sym = Symbol
+module TAst = TypedAst
 open Ast
 open Env
 
@@ -47,30 +47,29 @@ let rec infertype_expr env expr =
   | Ast.BinOp {left; op; right} -> infertype_binop env left op right
   | Ast.UnOp {op; operand} -> raise Unimplemented
   | Ast.Lval lvl -> infertype_lval env lvl
-  | Ast.Assignment {lvl; rhs} -> infertype_assignment env lvl rhs
+  | Ast.Assignment _ -> raise Unimplemented
   | Ast.Call _ -> raise Unimplemented
 and infertype_binop env left op right =
+    let left_texpr, left_tp = infertype_expr env left in
+    let right_texpr, right_tp = infertype_expr env right in
     match op with
     | Plus | Minus | Mul | Div | Rem | Lt | Le | Gt | Ge | Lor | Land -> 
       let expected_arg_typ = get_expected_binop_arg_typ op in
       let expected_res_typ = get_expected_binop_res_typ op in
-      let left_texpr = typecheck_expr env left expected_arg_typ in
-      let right_texpr = typecheck_expr env right expected_arg_typ in
-      (TAst.BinOp {left = left_texpr; op = typecheck_binop op; right = right_texpr; tp = expected_res_typ}, expected_res_typ)
-    | Eq | NEq ->
-      let right_texpr, right_tp = infertype_expr env right in
-      let left_texpr = typecheck_expr env left right_tp in
-      (TAst.BinOp {left = left_texpr; op = typecheck_binop op; right = right_texpr; tp = TAst.Bool}, TAst.Bool)
-and infertype_assignment env lvl rhs =
-  let _ , lvl_tp = infertype_lval env lvl in
-  let rhs_texpr, rhs_tp = infertype_expr env rhs in
-  let asgn_tp =
-    if rhs_tp = TAst.ErrorType then lvl_tp
-    else if lvl_tp = TAst.ErrorType then rhs_tp
-    else if lvl_tp = rhs_tp then lvl_tp
-    else let _ = Errors.TypeMismatch {expected = lvl_tp; actual = rhs_tp} in lvl_tp
-  in match lvl with Ast.Var Ast.Ident {name} ->
-    TAst.Assignment {lvl = TAst.Var {ident = TAst.Ident {sym = Sym.symbol name}; tp = lvl_tp}; rhs = rhs_texpr; tp = asgn_tp}, asgn_tp
+      if left_tp = expected_arg_typ && right_tp = expected_arg_typ
+      then
+        (TAst.BinOp {left = left_texpr; op = typecheck_binop op; right = right_texpr; tp = expected_res_typ}, expected_res_typ)
+      else
+        let _ = 
+          if left_tp <> expected_arg_typ then 
+            Env.insert_error env (Errors.TypeMismatch {expected = expected_arg_typ; actual = left_tp}) 
+        in
+        let _ =
+          if right_tp <> expected_arg_typ then
+            Env.insert_error env (Errors.TypeMismatch {expected = expected_arg_typ; actual = right_tp}) 
+        in
+        (TAst.BinOp {left = left_texpr; op = typecheck_binop op; right = right_texpr; tp = TAst.ErrorType}, expected_res_typ)
+    | Eq | NEq -> raise Unimplemented
 and infertype_lval env lvl =
   match lvl with 
   | Ast.Var Ast.Ident {name} -> 
@@ -82,16 +81,16 @@ and infertype_lval env lvl =
     | Some t ->
       match t with
       | VarTyp vt -> TAst.Lval (TAst.Var {ident = TAst.Ident {sym = Sym.symbol name}; tp = vt}), vt
-      | FunTyp _ -> 
-        let _ = Env.insert_error env (Errors.LValueInvalid {sym = Sym.symbol name}) in
-        TAst.Lval (TAst.Var {ident = TAst.Ident {sym = Sym.symbol name}; tp = TAst.ErrorType}), TAst.ErrorType 
+      | FunTyp ft -> 
+        match ft with TAst.FunTyp {ret; _} ->
+          let _ = Env.insert_error env (Errors.LValueInvalid {sym = Sym.symbol name}) in
+          TAst.Lval (TAst.Var {ident = TAst.Ident {sym = Sym.symbol name}; tp = TAst.ErrorType}), ret
 
 (* checks that an expression has the required type tp by inferring the type and comparing it to tp. *)
 and typecheck_expr env expr tp =
   let texpr, texprtp = infertype_expr env expr in
-  if texprtp <> tp && texprtp <> TAst.ErrorType && tp <> TAst.ErrorType
-  then let _ = Env.insert_error env (Errors.TypeMismatch {expected = tp; actual = texprtp}) in texpr
-  else texpr 
+  if texprtp <> tp then raise Unimplemented;
+  texpr
 
 (* should check the validity of a statement and produce the corresponding typed statement. Should use typecheck_expr and/or infertype_expr as necessary. *)
 let rec typecheck_statement env stm =
@@ -135,7 +134,7 @@ let rec typecheck_statement env stm =
 and typecheck_statement_seq env stms = raise Unimplemented
 
 (* the initial environment should include all the library functions, no local variables, and no errors. *)
-let initial_environment = raise Unimplemented
+let initial_environment = Env.make_env Library.library_functions
 
 (* should check that the program (sequence of statements) ends in a return statement and make sure that all statements are valid as described in the assignment. Should use typecheck_statement_seq. *)
 let typecheck_prog prg = raise Unimplemented
