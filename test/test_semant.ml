f2ccd997de82fa689df460ecb86cb2c742677e56
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-10-04 09:49:56 +0200

    refactor lib

--- test/test_semant.ml
+++ test/test_semant.ml
@@ -1,10 +1,10 @@
-open Dolphin_compiler.Ast
-open Dolphin_compiler.Semant
-open Dolphin_compiler.Errors
-module Pretty = Dolphin_compiler.Pretty
+open Lib.Ast
+open Lib.Semant
+open Lib.Errors
+module Pretty = Lib.Pretty
 (* open Dolphin_compiler.TypedPretty *)
 
-type test_case = string * Dolphin_compiler.Ast.program
+type test_case = string * Lib.Ast.program
 
 let prog_1: test_case = "prog_1: Empty program" , []
 let prog_2: test_case = "prog_2: Return bool", [ReturnStm {ret = Boolean {bool = true}}]
@@ -18,11 +18,11 @@ let print_err e = let _ = Printf.printf "%s\n" (error_to_string e) in ()
 let test_semant (name, p) =
   let tprog, errors = typecheck_prog p in
   let _ = Printf.printf "======================================\n" in
-  let _ = Printf.printf "Running test on %s\n" name in
+  let _ = Printf.printf "RUNNING TEST ON %s\n" name in
   let _ = PrintBox_text.output stdout (Pretty.program_to_tree p) in
-  let _ = Printf.printf "\nTyped Program: \n" in
+  let _ = Printf.printf "\nTYPED PROGRAM: \n" in
   let _ = PrintBox_text.output stdout (TPretty.program_to_tree tprog) in
-  let _ = Printf.printf "\nError list: \n" in
+  let _ = Printf.printf "\nERROR LIST: \n" in
   let _ = List.map print_err !errors in
   let _ = Printf.printf "\n" in ()
 
