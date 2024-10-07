c5829faa1b7fa3383260edeb0c5253d1ec1fa3ba
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-10-07 20:56:03 +0200

    add test_codegen

---
+++ test/test_codegen.ml
@@ -0,0 +1,36 @@
+open Lib.Ast
+open Lib.Semant
+open Lib.Errors
+module Pretty = Lib.Pretty
+(* open Dolphin_compiler.TypedPretty *)
+
+type test_case = string * Lib.Ast.program
+
+let prog_1: test_case = "prog_1: Return 0", [ReturnStm {ret = Integer {int = 0L}}]
+let prog_2: test_case = "prog_2: Return 1", [ReturnStm {ret = Integer {int = 1L}}]
+let prog_3: test_case = "prog_3: ",
+  [
+    ExprStm {expr = Some (BinOp {left = Integer {int = 0L}; op = Plus; right = Boolean {bool = false}})};
+    ReturnStm {ret = Lval (Var (Ident {name = "x"}))}
+  ]
+
+let print_err e = let _ = Printf.printf "%s\n" (error_to_string e) in ()
+let test_codegen (name, p) =
+  let tprog, errors = typecheck_prog p in
+  let _ = Printf.printf "======================================\n" in
+  let _ = Printf.printf "RUNNING TEST ON %s\n" name in
+  let _ = PrintBox_text.output stdout (TPretty.program_to_tree tprog) in
+  if List.length !errors <> 0
+  then 
+    let _ = Printf.printf "\nERROR LIST: \n" in
+    let _ = List.map print_err !errors in
+    let _ = Printf.printf "\n" in
+    ()
+  else 
+    let llprog = Lib.CodeGenerator.codegen_prog tprog in
+    let _ = Printf.printf "\nLLVM code: \n" in
+    let _ = Printf.printf "\n%s\n" (Lib.Ll.string_of_prog llprog) in
+    ()
+
+let progs = [prog_1; prog_2; prog_3]
+let _ = List.map test_codegen progs
