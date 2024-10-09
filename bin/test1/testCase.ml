277884d41ee37c4a78789779fe50dac48f8077e6
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-10-09 13:34:39 +0200

    rename symbol

--- bin/test1/testCase.ml
+++ bin/test1/testCase.ml
@@ -3,31 +3,29 @@ open Lib.Ast
 
 let test1: test_case = 
   "test1",
-  "all positive functionalities",
+  "basic positive functionalities",
   [
-    VarDeclStm {name = x; tp = None; body = Integer {int = 1L}};
-    VarDeclStm {name = y; tp = None; body = Assignment {lvl = Var x; rhs = BinOp {left = Lval(Var x); op = Plus; right = Integer {int = 2L}}}};
-    VarDeclStm {name = z; tp = None; body = BinOp {left = Lval (Var x); op = Plus; right = Lval (Var y)}};
-    VarDeclStm {name = t; tp = None; body = Call {fname = read_integer; args = []}};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var x)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var y)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var z)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var t)]})};
+    VarDeclStm {name = int_x; tp = None; body = Integer {int = 1L}};
+    VarDeclStm {name = int_y; tp = None; body = Assignment {lvl = Var int_x; rhs = BinOp {left = Lval(Var int_x); op = Plus; right = Integer {int = 2L}}}};
+    VarDeclStm {name = int_z; tp = None; body = BinOp {left = Lval (Var int_x); op = Plus; right = Lval (Var int_y)}};
+    VarDeclStm {name = int_t; tp = None; body = Call {fname = read_integer; args = []}};
     IfThenElseStm {
-      cond = BinOp {left = Lval (Var x); op = Eq; right = Integer {int = 1L}};
-      thbr = ExprStm {expr = Some (Assignment {lvl = Var t; rhs = Lval (Var y)})};
+      cond = BinOp {left = Lval (Var int_x); op = Eq; right = Integer {int = 3L}};
+      thbr = ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_t)]})};
       elbro = None;
     };
+    VarDeclStm {name = int_u; tp = None; body = Integer {int = 0L}};
     IfThenElseStm {
-      cond = BinOp {left = Lval (Var x); op = Eq; right = Integer {int = 1L}};
-      thbr = ExprStm {expr = Some (Assignment {lvl = Var t; rhs = Lval (Var y)})};
-      elbro = Some (ExprStm {expr = Some (Assignment {lvl = Var t; rhs = Lval (Var z)})});
+      cond = BinOp {left = Lval (Var int_t); op = Le; right = Lval (Var int_x)};
+      thbr = ExprStm {expr = Some (Assignment {lvl = Var int_u; rhs = Lval (Var int_z)})};
+      elbro = Some (ExprStm {expr = Some (Assignment {lvl = Var int_u; rhs = Lval (Var int_t)})});
     };
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var x)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var y)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var z)]})};
-    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var t)]})};
-    ReturnStm {ret = Lval (Var t)}
+    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_x)]})};
+    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_y)]})};
+    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_z)]})};
+    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_t)]})};
+    ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_u)]})};
+    ReturnStm {ret = Lval (Var int_u)}
   ]
 
 let _ = run_testcase test1
