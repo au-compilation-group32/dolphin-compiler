54d53f12ffa2194e6a7dcc065253e9c0d7d79a29
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-10-09 22:01:54 +0200

    add check for void type

---
+++ bin/test9/testCase.ml
@@ -0,0 +1,16 @@
+open Lib.TestUtils
+open Lib.Ast
+
+let test9: test_case = 
+  "test9",
+  "mics negative test",
+  [
+    VarDeclStm {name = int_x; tp = None; body = Integer {int = 419L}};
+    ExprStm {expr = Some (Assignment {lvl = Var int_x; rhs = Call {fname = print_integer; args = [Lval(Var int_x)]}})};
+    VarDeclStm {name = int_y; tp = None; body = Call {fname = print_integer; args = [Lval(Var int_x)]}};
+    VarDeclStm {name = int_y; tp = Some Int; body = Call {fname = print_integer; args = [Lval(Var int_x)]}};
+
+    ReturnStm {ret = Boolean {bool = true}}
+  ]
+
+let _ = run_testcase test9
