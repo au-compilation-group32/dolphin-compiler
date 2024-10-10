554e541a32175cbc88baaec3ccce49b5daf7ce8a
Laurits Vest Hansen <202204246@post.au.dk>
2024-10-10 11:10:01 +0000

    make old test work

--- bin/test1/testCase.ml
+++ bin/test1/testCase.ml
@@ -4,17 +4,23 @@ open Lib.Ast
 let test1: test_case = 
   "test1",
   "basic positive functionalities",
+  let delc1 = Declaration {name = int_x; tp = None; body = Integer {int = 1L}} in
+  let delc2 = Declaration {name = int_y; tp = None; body = Assignment {lvl = Var int_x; rhs = BinOp {left = Lval(Var int_x); op = Plus; right = Integer {int = 2L}}}} in
+  let delc3 = Declaration {name = int_z; tp = None; body = BinOp {left = Lval (Var int_x); op = Plus; right = Lval (Var int_y)}} in
+  let delc4 = Declaration {name = int_t; tp = None; body = Call {fname = read_integer; args = []}} in
+  let delc_block = [delc1; delc2; delc3; delc4] in
+  let declar_block : declaration_block = DeclBlock delc_block in
+  let delc5 = Declaration {name = int_u; tp = None; body = Integer {int = 0L}} in
+  let delc_block2 = [delc5] in
+  let declar_block2 : declaration_block = DeclBlock delc_block2 in
   [
-    VarDeclStm {name = int_x; tp = None; body = Integer {int = 1L}};
-    VarDeclStm {name = int_y; tp = None; body = Assignment {lvl = Var int_x; rhs = BinOp {left = Lval(Var int_x); op = Plus; right = Integer {int = 2L}}}};
-    VarDeclStm {name = int_z; tp = None; body = BinOp {left = Lval (Var int_x); op = Plus; right = Lval (Var int_y)}};
-    VarDeclStm {name = int_t; tp = None; body = Call {fname = read_integer; args = []}};
+    VarDeclStm declar_block;
     IfThenElseStm {
       cond = BinOp {left = Lval (Var int_x); op = Eq; right = Integer {int = 3L}};
       thbr = ExprStm {expr = Some (Call {fname = print_integer; args = [Lval(Var int_t)]})};
       elbro = None;
     };
-    VarDeclStm {name = int_u; tp = None; body = Integer {int = 0L}};
+    VarDeclStm declar_block2;
     IfThenElseStm {
       cond = BinOp {left = Lval (Var int_t); op = Le; right = Lval (Var int_x)};
       thbr = ExprStm {expr = Some (Assignment {lvl = Var int_u; rhs = Lval (Var int_z)})};
