c59f568c6d4c4d7f946f314ae4b3812bd3b5d509
Laurits Vest Hansen <202204246@post.au.dk>
2024-10-10 17:46:47 +0000

    last tests

--- bin/test11/testCase.ml
+++ bin/test11/testCase.ml
@@ -4,11 +4,14 @@ open Lib.Ast
 let test11: test_case = 
   "test11",
   "nested function call",
+  let delc11 = Declaration {name = int_x; tp = None; body = Integer {int = 100L}} in
+  let delc21 = Declaration {name = int_y; tp = None; body = Integer {int = 69L}} in
+  let delc31 = Declaration {name = int_z; tp = None; body = Integer {int = 419L}} in
+  let delc41 = Declaration {name = int_t; tp = None; body = Integer {int = 10L}} in
+  let delc_block4 = [delc11; delc21; delc31; delc41] in
+  let declar_block4 : declaration_block = DeclBlock delc_block4 in
   [
-    VarDeclStm {name = int_x; tp = None; body = Integer {int = 100L}};
-    VarDeclStm {name = int_y; tp = None; body = Integer {int = 69L}};
-    VarDeclStm {name = int_z; tp = None; body = Integer {int = 419L}};
-    VarDeclStm {name = int_t; tp = None; body = Integer {int = 10L}};
+    VarDeclStm declar_block4;
 
     ExprStm {expr = Some (
       Assignment {
--- bin/test9/testCase.ml
+++ bin/test9/testCase.ml
@@ -4,11 +4,17 @@ open Lib.Ast
 let test9: test_case = 
   "test9",
   "mics negative test",
+  let delc11 = Declaration {name = int_x; tp = None; body = Integer {int = 419L}} in
+  let delc_block1 = [delc11] in
+  let declar_block1 : declaration_block = DeclBlock delc_block1 in
+  let delc21 = Declaration {name = int_y; tp = None; body = Call {fname = print_integer; args = [Lval(Var int_x)]}} in
+  let delc31 = Declaration {name = int_y; tp = Some Int; body = Call {fname = print_integer; args = [Lval(Var int_x)]}} in
+  let delc_block3 = [delc21; delc31] in
+  let declar_block3 : declaration_block = DeclBlock delc_block3 in
   [
-    VarDeclStm {name = int_x; tp = None; body = Integer {int = 419L}};
+    VarDeclStm declar_block1;
     ExprStm {expr = Some (Assignment {lvl = Var int_x; rhs = Call {fname = print_integer; args = [Lval(Var int_x)]}})};
-    VarDeclStm {name = int_y; tp = None; body = Call {fname = print_integer; args = [Lval(Var int_x)]}};
-    VarDeclStm {name = int_y; tp = Some Int; body = Call {fname = print_integer; args = [Lval(Var int_x)]}};
+    VarDeclStm declar_block3;
 
     ExprStm {expr = Some (Assignment {lvl = Var int_x; rhs = BinOp{left = Call {fname = print_integer; args = [Lval(Var int_x)]}; op = Eq; right = Lval(Var int_x)}})};
 
