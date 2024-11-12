9fbd84f8e50359c0b07eb85e8e45505753a38c79
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-11-13 00:51:49 +0100

    supress output when run all tests

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-NUM_TEST=43
+NUM_TEST=44
 
 for i in $(seq 1 $NUM_TEST)
 do
-    bash run_test.sh test/test$i
+    bash run_test.sh test/test$i >/dev/null 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "test$i failed"
         exit 1
+    else
+        echo "test$i succeeded"
     fi
 done
 
